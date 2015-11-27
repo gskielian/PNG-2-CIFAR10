baa5efd71afdc9f77247179a1169ee6e336a1148
gkielian@google.com <gkielian@google.com>
2015-11-27 15:13:37 -0800

    add resize-script.sh -- imagemagick based

---
+++ resize-script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#simple script for resizing images in all class directories
+for file in classes/*/*.png; do
+  convert "$file" -resize 32x32\! "$file"
+  #file "$file" #uncomment for testing
+done
+
