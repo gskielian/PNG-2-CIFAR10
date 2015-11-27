73440ea29e057a3d256d6c37d26ee280c6d27b87
gkielian@google.com <gkielian@google.com>
2015-11-27 15:23:46 -0800

    add verification to resize-script.sh

--- resize-script.sh
+++ resize-script.sh
@@ -3,6 +3,6 @@
 #simple script for resizing images in all class directories
 for file in classes/*/*.png; do
   convert "$file" -resize 32x32\! "$file"
-  #file "$file" #uncomment for testing
+  file "$file" #uncomment for testing
 done
 
