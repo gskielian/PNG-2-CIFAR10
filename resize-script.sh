b4804d05f52192f12f9664c5b7e4ed337e0b80c4
Gregory Kielian <gregory.kielian@gmail.com>
2015-12-29 10:22:54 +0100

    debug jpg2png conversion; handling case of no file

--- resize-script.sh
+++ resize-script.sh
@@ -2,8 +2,37 @@
 
 #simple script for resizing images in all class directories
 #also reformats everything from whatever to png
-for file in classes/*/*.png; do
-  convert "$file" -resize 28x28\! "${file%.*}png"
-  file "$file" #uncomment for testing
-done
 
+if [ `ls test-images/*/*.jpg 2> /dev/null | wc -l ` -gt 0 ]; then
+  echo hi
+  for file in test-images/*/*.jpg; do
+    convert "$file" -resize 28x28\! "${file%.*}.png"
+    file "$file" #uncomment for testing
+    rm "$file"
+  done
+fi
+
+if [ `ls test-images/*/*.png 2> /dev/null | wc -l ` -gt 0 ]; then
+  echo hi
+  for file in test-images/*/*.png; do
+    convert "$file" -resize 28x28\! "${file%.*}.png"
+    file "$file" #uncomment for testing
+  done
+fi
+
+if [ `ls training-images/*/*.jpg 2> /dev/null | wc -l ` -gt 0 ]; then
+  echo hi
+  for file in training-images/*/*.jpg; do
+    convert "$file" -resize 28x28\! "${file%.*}.png"
+    file "$file" #uncomment for testing
+    rm "$file"
+  done
+fi
+
+if [ `ls training-images/*/*.png 2> /dev/null | wc -l ` -gt 0 ]; then
+  echo hi
+  for file in training-images/*/*.png; do
+    convert "$file" -resize 28x28\! "${file%.*}.png"
+    file "$file" #uncomment for testing
+  done
+fi
