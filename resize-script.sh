373252edfd670d6396f8dc9053f27e1b23a1a620
Gregory Kielian <gregory.kielian@gmail.com>
2015-12-29 05:38:14 +0100

    upgraded to also convert files to png

--- resize-script.sh
+++ resize-script.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 #simple script for resizing images in all class directories
+#also reformats everything from whatever to png
 for file in classes/*/*.png; do
-  convert "$file" -resize 32x32\! "$file"
+  convert "$file" -resize 28x28\! "${file%.*}png"
   file "$file" #uncomment for testing
 done
 
