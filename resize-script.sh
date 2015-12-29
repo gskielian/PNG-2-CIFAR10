7442863a1c0186b6ff3e55c191284dc457281827
Gregory S. Kielian <gregory.kielian@gmail.com>
2015-12-29 10:46:58 +0100

    convert to png from jpg, test for empty dirs

--- resize-script.sh
+++ resize-script.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
 #simple script for resizing images in all class directories
-for file in classes/*/*.png; do
-  convert "$file" -resize 32x32\! "$file"
-  file "$file" #uncomment for testing
-done
+#also reformats everything from whatever to png
 
+if [ `ls classes/*/*.jpg 2> /dev/null | wc -l ` -gt 0 ]; then
+  for file in test-images/*/*.jpg; do
+    convert "$file" -resize 32x32\! "${file%.*}.png"
+    file "$file" #uncomment for testing
+    rm "$file"
+  done
+fi
+
+if [ `ls classes/*/*.png 2> /dev/null | wc -l ` -gt 0 ]; then
+  for file in test-images/*/*.png; do
+    convert "$file" -resize 32x32\! "${file%.*}.png"
+    file "$file" #uncomment for testing
+  done
+fi
