0c831981545848014444fd034b5b06f2d5f935d0
Gregory Kielian <gregory.kielian@gmail.com>
2015-12-29 06:49:27 +0100

    add labels/training byte header, & append loops

--- convert-images-to-cifar-format.py
+++ convert-images-to-cifar-format.py
@@ -1,63 +1,76 @@
-#python script for converting 32x32 pngs to format
+#resize script converts images to 28x28 px pngs
+#this python script for converting 28x28 pixel pngs to MNIST format
 
 from PIL import Image
 import os
 from array import *
 
-data = array('B')
+folders = ['./training-images', './test-images']
 
-for dirname, dirnames, filenames in os.walk('./classes'):
+for folder in folders:
+  #reinit data
+  data_image = array('B')
+  data_label = array('B')
+
+  #TODO get this into a sub-routine
+  #set headers one byte at a time
+  data_image.append(0x00)
+  data_image.append(0x00)
+  data_image.append(0x08)
+  data_image.append(0x03) # magic number for training images
+
+  data_image.append(0x00)
+  data_image.append(0x00)
+  data_image.append(0x00)
+  data_image.append(0x02) # two images, TODO replace with variable
+
+  data_image.append(0x00)
+  data_image.append(0x00)
+  data_image.append(0x00)
+  data_image.append(0x1C) # number of rows 28
+
+  data_image.append(0x00)
+  data_image.append(0x00)
+  data_image.append(0x00)
+  data_image.append(0x1C) # number of columnns 28
+
+  #TODO get this into a sub-routine
+  data_label.append(0x00)
+  data_label.append(0x00)
+  data_label.append(0x08)
+  data_label.append(0x01) # magic number for training images
+
+  data_label.append(0x00)
+  data_label.append(0x00)
+  data_label.append(0x00)
+  data_label.append(0x02) # two images, TODO replace with variable
+
+  for dirname, dirnames, filenames in os.walk(folder):
     for filename in filenames:
-        if filename.endswith('.png'):
+      if filename.endswith('.png'):
 
-            ################
-            #grab the image#
-            ################
+        im = Image.open(os.path.join(dirname, filename))
+        pix = im.load()
+        #print(os.path.join(dirname, filename))
 
-            im = Image.open(os.path.join(dirname, filename))
-            pix = im.load()
-            #print(os.path.join(dirname, filename))
+        #store the class name from look at path
+        class_name = int(os.path.join(dirname).split('/')[-1])
+        #print class_name
 
-            #store the class name from look at path
-            class_name = int(os.path.join(dirname).split('/')[-1])
-            #print class_name
+        ###############################
+        #        MNIST Fork           #
+        #getting image into byte array#
+        ###############################
 
 
+        #append image
+        for x in range(0,28):
+          for y in range(0,28):
+            print average(pix[x,y])
+            data_image.append(average(pix[x,y]))
 
-
-            ###############################
-            #        MNIST Fork           #
-            #getting image into byte array#
-            ###############################
-
-            # create array of bytes to hold stuff
-            data.append(0x0803) # magic number for training images
-            data.append(0x0002) # two images
-            data.append(0x001C) # number of rows 28
-            data.append(0x001C) # number of columnns 28
-
-            # create array of bytes to hold stuff
-            data.append(0x0801) # magic number for training labels
-            data.append(0x0002) # two images
-            data.append(0x001C) # number of rows 28
-            data.append(0x001C) # number of columnns 28
-
-            #first append the class_name byte
-            data.append(class_name)
-
-            #then write the rows
-
-            #Pixels are organized row-wise. Pixel values are 0 to 255. 0 means background (white), 255 means foreground (black).
-
-            #note: rows then columns
-            #note: no delimters, just grayscale images left right, top down
-            #note: each pixel is one byte each
-            #note: each label is also one byte each
-            for color in range(0,3):
-                for x in range(0,28):
-                    for y in range(0,28):
-                        data.append(pix[x,y][color])
-
+        #append label
+        data_label.append(class_name) # labels start (one unsigned byte each)
 
 ############################################
 #write all to binary, all set for cifar10!!#
@@ -68,3 +81,5 @@ data.tofile(output_file)
 output_file.close()
 
 
+def average(pixel):
+  return (pixel[0] + pixel[1] + pixel[2])/3
